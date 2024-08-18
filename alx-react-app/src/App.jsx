a6dbbac73d56b676d2fcd41eb90737d51ed48a54
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-08-18 23:45:13 +0000

    first react app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-import WelcomeMessage from './WelcomeMessage';
+import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
