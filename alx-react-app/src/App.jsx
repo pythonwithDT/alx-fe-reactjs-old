2bb8a58d6d89d3ed7dab9e01300497a7cd0fd20c
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-08-18 23:47:52 +0000

    first react app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
@@ -38,6 +39,13 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+
     </>
   );
 }
